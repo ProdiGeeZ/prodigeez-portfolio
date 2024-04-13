e40f287d192b001bcfccacaf2ad646c358170371
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-13 18:02:51 +0100

    Adjust responsiveness for mobile and desktop

--- src/components/routes/home/About.jsx
+++ src/components/routes/home/About.jsx
@@ -1,7 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { Button } from "@/components/ui/button"
+import { ChevronRight } from 'lucide-react';
 
 function About() {
     return (
-        <div>About</div>
+        <div className="w-full flex flex-col mt-12 md:flex-row px-10 py-12 justify-center h-auto md:h-[100vh] md:px-80 md:pt-[150px]">
+            <div className="w-full h-full align-self-center md:mr-10">
+                <div className="w-full md:p-20 lg:p-0 flex ">
+                    <img
+                        className="w-[100%] h-auto rounded-lg"
+                        src="./about.jpg"
+                        alt="image of myself"
+                    />
+                </div>
+            </div>
+            <div className="w-full mt-4">
+                <h1 className="text-white font-bold text-3xl mb-4 lg:mt-[125px]">
+                    About Me
+                </h1>
+                <h3 className='font-bold text-xl'> Based in Manchester, England<span role="img" aria-label="Location"> 📍</span></h3>
+                <p className="mb-6 leading-relaxed text-base">
+                    I'm a junior full stack developer with a passion for technology and creativity. I'm currently seeking an entry-level role in software development. I acquired a bachelor’s degree in Software Engineering and then completed a comprehensive Full-Stack bootcamp.
+                </p>
+                <h3><span role="img" aria-label="Star" className='font-bold text-xl'>Hobbies & Interests</span></h3>
+                <p className="mb-6 leading-relaxed text-base">
+                    From a young age, I've been fascinated by creative arts, exploring photography, videography, and graphic design through school and beyond.
+                </p>
+                <Button href="/portfolio" className="inline-flex items-center justify-center w-44 md:w-auto px-6 py-2 md:text-base text-md rounded-md font-semibold lg:text-lg lg:mb-6">
+                    See Portfolio <ChevronRight aria-label="Right arrow" />
+                </Button>
+            </div>
+        </div>
     )
 }
 
--- src/components/routes/home/Home.jsx
+++ src/components/routes/home/Home.jsx
@@ -1,33 +1,44 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ChevronDown } from 'lucide-react';
 import { Button } from "@/components/ui/button"
+import About from "./About";
 
 
 function Home() {
     return (
-        <div className="w-full h-full flex items-center justify-center">
-            <div className="text-center w-full h-full flex flex-col items-center p-1 md:p-20">
-                <Avatar className="rounded-full w-32 h-32 mb-6 mt-6 sm:w-24 sm:h-24 md:w-40 md:h-40 md:mb-2 lg:mb-0 border-4 border-[#dc26269d] lg:w-48 lg:h-48 shadow-xl shadow-red-500/50 " style={{ boxShadow: '0px 2px 10px 4px rgba(255, 0, 0, 0.5)'}}>
-                    <AvatarImage src="./avatar.png" />
-                    <AvatarFallback>SA</AvatarFallback>
-                </Avatar>
-                <h1 className="text-4xl md:text-5xl lg:text-7xl lg:mb-6 font-bold md:pt-4 lg:pt-6 mb-3 text-primary">Hi, I'm Saabir</h1>
-                <h2 className="text-l md:text-2xl text-foreground font-bold lg:mb-4">Junior Full Stack Developer</h2>
-                <p className="text-base md:text-lg mb-4 m-2 p-1 lg:mb-6">
-                    Full stack developer based in Manchester, England. Obsessed with the creative process of software development.
-                </p>
-                <Button className="md:text-base py-4 px-4 text-md rounded-md font-semibold hover:bg-[#fd8d8d] hover:text-[#fff] lg:py-6 lg:px-6 lg:text-lg lg:mb-6 ">
-                    View My Work
-                </Button>
-                <div className="flex flex-wrap justify-center mt-6 bg-[#67000067] w-70 px-5 py-2 items-center gap-2 md:gap-4 md:mt-4 lg:w-70 border-[#494949b7] border-2 rounded-full p-1 hover:bg-[#670000a9] transition-all ">
-                    <h6 className="text-md md:text-lg lg:text-xl mb-0 font-bold flex items-center">Tech Stack</h6>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node" className="w-6 h-6 md:w-8 md:h-8 hover:scale-125 transition-all" />
-                    <i className="devicon-express-original text-xl lg:text-[32px] hover:scale-125 transition-all"></i>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React" className="w-6 h-6 md:w-8 md:h-8 hover:scale-125 transition-all" />
-                    <i className="devicon-postgresql-plain-wordmark text-xl lg:text-[32px] hover:scale-125 transition-all"></i>
+        <>
+            <div className="w-full h-full flex items-center justify-center">
+                <div className="text-center w-full h-[95vh] pt-12 mt-8 md:mt-0 flex flex-col items-center p-1 md:p-20">
+                    <Avatar
+                        className="rounded-full w-[200px] h-[200px] mb-6 sm:w-24 sm:h-24 md:w-full md:h-auto md:mb-2 lg:mb-0 dark:border-4 dark:border-[#dc26269d] lg:w-48 lg:h-48">
+                        <AvatarImage src="./avatar.png" />
+                        <AvatarFallback>SA</AvatarFallback>
+                    </Avatar>
+                    <h1 className="text-5xl md:text-7xl lg:text-7xl lg:mb-6 font-bold md:pt-4 lg:pt-6 mb-3 text-primary">Hi, I'm Saabir</h1>
+                    <h2 className="text-xl md:text-2xl lg:text-3xl text-foreground font-bold lg:mb-4">Junior Software Engineer</h2>
+                    <p className="text-base font-semibold md:text-lg p-6 lg:mb-6 ">
+                        Eager developer looking to learn, contribute, and grow in vast world of technology.
+                    </p>
+                    <Button className="w-[35%] h-[6%] mb-12 md:w-auto md:text-base text-md rounded-md font-semibold hover:bg-[#fd8d8d] hover:text-white lg:text-lg lg:mb-20">
+                        View My Work
+                    </Button>
+                    <div className="flex flex-wrap justify-center w-70 px-5 py-2 items-center gap-2 md:gap-4 md:mt-4 lg:w-70 border-[#494949b7] border-2 rounded-full p-1 hover:bg-[#670000a9] transition-all root:bg-transparent">
+                        <h6 className="text-md md:text-lg lg:text-xl mb-0 font-bold flex items-center">Tech Stack</h6>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                            alt="Node"
+                            className="w-6 h-6 md:w-8 md:h-8 hover:scale-125 transition-all" />
+                        <i className="devicon-express-original text-xl lg:text-[32px] hover:scale-125 transition-all"></i>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                            alt="React"
+                            className="w-6 h-6 md:w-8 md:h-8 hover:scale-125 transition-all" />
+                        <i className="devicon-postgresql-plain-wordmark text-xl lg:text-[32px] hover:scale-125 transition-all"></i>
+                    </div>
+                    <ChevronDown className="align-self-center mt-20 md:mt-5 hover:scale-125 hover:text-[#b30000] transition-all" size="45px" />
                 </div>
             </div>
-        </div>
+            <About />
+        </>
     )
 }
 
