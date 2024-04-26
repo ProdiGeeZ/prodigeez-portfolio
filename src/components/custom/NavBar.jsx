7fd8cbbe5be32bc0aba08205d2c2657ceb7ceae2
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-26 16:22:56 +0100

    make navbar position absolute to balance layout

--- src/components/custom/NavBar.jsx
+++ src/components/custom/NavBar.jsx
@@ -8,27 +8,25 @@ import { Menu } from 'lucide-react';
 
 function NavBar() {
     return (
-        <header className="max-w-screen">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-                <div className="flex justify-between items-center py-6 md:justify-start">
-                    <div className="flex justify-start lg:w-0 lg:flex-1">
-                        <a href="/" className="text-xl md:text-2xl font-bold text-primary transition-all" >
-                            <span className="inline-flex p-0 m-0 items-center">
-                                <img src="https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/personal/favicon.svg?t=2024-04-19T17%3A59%3A28.415Z" width={50} className="p-0 m-0" />
-                                <p className="">SAABIR.DEV</p>
-                            </span>
+        <header className="absolute w-full overflow-x-hidden">
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6">
+                <div className="flex justify-between items-center">
+                    <div className="flex justify-start lg:flex-1">
+                        <a href="/" className="flex items-center text-xl md:text-2xl font-bold text-primary transition-all">
+                            <img src="https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/personal/favicon.svg" alt="logo" width={50} />
+                            <p className="ml-2 pt-1">SAABIR.DEV</p>
                         </a>
                     </div>
-                    <div className="md:hidden flex space-x-2 ">
-                        <ModeToggle />
-                        <DropdownMenu className="w-full mr-10">
+                    <div className="flex items-center space-x-2 md:hidden">
+                    <ModeToggle />
+                        <DropdownMenu className="w-full">
                             <DropdownMenuTrigger asChild>
                                 <Button variant="outline" size="icon">
                                     <Menu />
                                 </Button>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
-                                <DropdownMenuItem >
+                                <DropdownMenuItem>
                                     <Link to="/projects">Projects</Link>
                                 </DropdownMenuItem>
                                 <DropdownMenuItem href="/contact">
@@ -39,15 +37,13 @@ function NavBar() {
                     </div>
                     <NavigationMenu className="hidden md:flex items-center justify-end lg:w-0">
                         <NavigationMenuList className="flex space-x-8">
-                            <NavigationMenuItem>
-                                <NavigationMenuLink href="/projects" className="text-base font-medium">
+                            <NavigationMenuLink href="/projects" className="text-base font-medium">
                                     Projects
                                 </NavigationMenuLink>
-                            </NavigationMenuItem>
                             <NavigationMenuItem>
-                                    <Scroll to="about" spy={true} smooth={true} duration={500} className="text-base font-medium cursor-pointer self-center">
-                                        About
-                                    </Scroll>
+                                <Scroll to="about" spy={true} smooth={true} duration={500} className="text-base font-medium cursor-pointer self-center">
+                                    About
+                                </Scroll>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <Button variant='default' className="transition ease-in-out">
