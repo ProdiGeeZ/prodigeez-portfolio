dedc5da854e0d1a2f3152e083974783b0cf3f1eb
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-27 15:56:39 +0100

    fix path error for nested routes loading screen

--- src/components/custom/LoadingScreen.jsx
+++ src/components/custom/LoadingScreen.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { Progress } from "../ui/progress";
 
 const images = [
-    './1.svg', './2.svg', './3.svg', './4.svg', './5.svg',
-    './6.svg', './7.svg', './8.svg', './9.svg', './10.svg',
-    './11.svg', './12.svg', './13.svg', './14.svg', './15.svg'
+    '/./1.svg', '/./2.svg', '/./3.svg', '/./4.svg', '/./5.svg',
+    '/./6.svg', '/./7.svg', '/./8.svg', '/./9.svg', '/./10.svg',
+    '/./11.svg', '/./12.svg', '/./13.svg', '/./14.svg', '/./15.svg'
 ];
 
 function LoadingScreen() {
