fc9ce2e796604719b99bab277d1de9e234944fc8
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-27 14:17:16 +0100

    clean up intervals for smoother transitions

--- src/components/custom/LoadingScreen.jsx
+++ src/components/custom/LoadingScreen.jsx
@@ -13,19 +13,30 @@ function LoadingScreen() {
         'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/laugh%20cry.png',
         'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/ninja%20run.png',
         'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/sleepy%20coffee.png',
-        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/tough%20walk.png'
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/tough%20walk.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/homework.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/i%20hate%20homework.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/laptop%20cat.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/spit%20take.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/steam%20coming%20out%20your%20ears.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/rock.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/why.png',
+        'https://waltibmozphnocxzjzxf.supabase.co/storage/v1/object/public/loading/bruh.png',
     ];
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const randomIndex = Math.floor(Math.random() * images.length);
+            let nextIndex;
+            do {
+                nextIndex = Math.floor(Math.random() * images.length);
+            } while (nextIndex === index);
+            setIndex(nextIndex);
             setBlink(true);
-            setTimeout(() => setBlink(false), 250);
-            setIndex(randomIndex);
-        }, 300);
+            setTimeout(() => setBlink(false), 200);
+        }, 250);
 
         return () => clearInterval(intervalId);
-    }, [images.length]);
+    }, [index, images.length]);
 
     useEffect(() => {
         const progressInterval = setInterval(() => {
@@ -37,11 +48,12 @@ function LoadingScreen() {
                 return oldProgress + 1.25;
             });
         }, 25);
+
         return () => clearInterval(progressInterval);
     }, []);
 
     return (
-        <div className="w-screen h-screen flex justify-center items-center ">
+        <div className="w-screen h-screen flex justify-center items-center">
             <div className="w-[350px] h-auto flex flex-col justify-center items-center">
                 <img src={images[index]} className={`w-[300px] h-auto ${blink ? 'blink' : ''}`} alt="Loading image" />
                 <p className="text-4xl font-bold text-animate py-2">Loading...</p>
