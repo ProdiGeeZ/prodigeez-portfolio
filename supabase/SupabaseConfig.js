fa1ee50f458c79db54ad59deaedfabbed7bbbeb0
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-28 02:53:18 +0100

    change supabase config

--- supabase/SupabaseConfig.js
+++ supabase/SupabaseConfig.js
@@ -1,3 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+// SupabaseConfig.js
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(import.meta.env.VITE_API_URL, import.meta.env.VITE_ANON_KEY)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export { supabase };
